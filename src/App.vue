0d0c1f06b03e9284e9a7d609ecc506d385f023a9
Brice Friha <brice.friha@aol.fr>
2020-02-29 16:10:38 +0100

    Create a loop to display employees

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <h1>Employees</h1>
 
     <!-- Import our table -->
-    <employee-table />
+    <employee-table :employees="employees"/>
   </div>
 </template>
 
@@ -14,9 +14,32 @@ import EmployeeTable  from '@/components/EmployeeTable.vue'
 export default {
   name: 'app',
   components: {
-    EmployeeTable
-  }
+    EmployeeTable,
+  },
+  
+data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Richard Hendricks',
+          email: 'richard@piedpiper.com',
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        },
+      ],
+    }
+  },
 }
+
 </script>
 
 <style>
